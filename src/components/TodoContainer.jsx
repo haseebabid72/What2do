6019b5fb541f3807cff3ed5eaeb165731ab00ae9
Your Name <haseebabid72@outlook.com>
2024-03-12 02:16:42 +0500

    modified:   src/components/TodoContainer.jsx

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import "./TodoContainer.css"
+import { useState } from 'react'
 
 const TodoContainer = () => {
+
+  const [todos, settodos] = useState([
+    {
+      title: "Hey",
+    }
+  ])
     
   return (
     <>
       <div className="MainCont border border-white w-11/12 min-h-96 mx-9 p-4">
-        shit
+        <div className="maininput flex justify-center gap-4">
+          <input type="text" name="Enter Todo" id="todoinput" />
+        </div>
+        {todos.map(todo => {
+          return (
+          <>
+            <div className="todos">{todo.title}</div>
+          </>
+          )
+        })}
       </div>
     </>
   )

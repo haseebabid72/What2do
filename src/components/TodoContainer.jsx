8211ea83c4ff506d5e2914c2da7f85403026697c
Mudasar <mudasarmajeed5@yahoo.com>
2024-03-12 16:14:01 -0700

    Edit stuff is remaining.. also updating status when the user checks the box is not handled..

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -4,37 +4,33 @@ import { useState } from 'react'
 import { MdDelete } from "react-icons/md";
 import { FaPenNib } from "react-icons/fa";
 const TodoContainer = () => {
+  // Allows user to directly add task by pressing Enter key if the length of the input is greater than 5 characters.
+  const [isDisabled, setisDisabled] = useState(true)
   const [todos, settodos] = useState([]);
   const [Todo, setTodo] = useState("")
 
   const newtodo = (e) =>{
     setTodo(e.target.value)
+    e.target.value.length>5?setisDisabled(false):setisDisabled(true);
+    return isDisabled;
   }
-
+  
   const Adder = () =>{
     settodos([...todos, {Todo, status: false}])
-    setTodo("")
+    setTodo("");
+    setisDisabled(true);
   }
-
-  const DeleteTodo = (index) =>{
-    // todos.filter will return an array of the Todos that are the ones except the current indexed Todo in the array.
-    
-    // set_todos function Will return an array of the Todos except the one of index
-    const set_todos = () => {
-      let DeleteTodosAtIndex = todos.splice(index,1);
-      console.log(todos);
-      if (todos.length == 0){
-        settodos([])
-        // Problem is that the last todo is not getting removed, also if there are only 2 Todos and user clicks on the delete button of the first Todo.. it will delete the second Todo and also the last one is not removed from the list.
-        
-        console.log('0 todos returns empty array.');
-      }
-      return DeleteTodosAtIndex
-    }
-    let newTodos = todos.filter(set_todos);
+  const DeleteTodo = (index)=>{
+    let newTodos = todos.filter((_, i) => i !== index);
     settodos(newTodos)
   }
-
+  const Complete_Status = (e,index) => {
+    let addClass = document.getElementsByClassName("line_through")[index];
+    e.target.checked ? addClass.classList.add("line-through"):addClass.classList.remove("line-through");
+  }
+ 
+  
+  
   return (
     <>
       <div className="MainCont bg-[#1c2864] rounded-md w-11/12 min-h-96 mx-9 p-2 md:p-4">
@@ -44,7 +40,7 @@ const TodoContainer = () => {
           </div>
           <div className='w-[100%] flex justify-center items-center gap-5'>
             <input onChange={newtodo} value={Todo} type="text" name="Enter Todo" id="todoinput" className='w-[80%] md:w-[40%] bg-transparent border border-[teal] rounded-lg px-2 py-1' />
-            <button onClick={Adder} className='bg-[teal] px-4 py-1 rounded-sm text-white hover:bg-teal-400 transition-all'>SET</button>
+            <button onClick={Adder} disabled={isDisabled} id='Adder' className='bg-[teal] px-4 py-1 rounded-sm text-white hover:bg-teal-400 transition-all disabled:bg-gray-700'>SET</button>
           </div>
         </div>
         <div className="mt-5">
@@ -52,7 +48,7 @@ const TodoContainer = () => {
         {todos.map((todo, index) => {
           return (
             <div key={index} className="todos flex justify-between md:w-[70%] w-full mt-1 mx-auto hover:bg-teal-600  transition-all bg-[teal] px-2 py-1 rounded-sm">
-              <div>{todo.Todo}</div>
+              <div className='flex items-center gap-2'><input onChange={(e)=>{Complete_Status(e,index)}} type="checkbox" name="" /><span className='line_through'>{todo.Todo}</span></div>
               <div className='flex items-center gap-2'>
               <button className='bg-red-700 transition-all duration-300 p-1 hover:bg-black'><FaPenNib/></button>
               <button onClick={()=>{DeleteTodo(index)}} className='bg-red-700 transition-all duration-300 p-1 hover:bg-black'><MdDelete/></button>
